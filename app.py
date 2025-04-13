405966185ac08ebcc45b0baa89c9bcb57bd00546
Abdulkareem Olumoh <abdulkareemolumoh@protonmail.com>
2025-04-13 22:37:58 +0100

    feat: Enable debug mode for auto-reloading in app.run()

--- app.py
+++ app.py
@@ -4,28 +4,21 @@ from flask_cors import CORS, cross_origin
 from kidneyClassifier.utils.common import decodeImage
 from kidneyClassifier.pipeline.prediction import PredictionPipeline
 
-
-
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
 CORS(app)
 
-
 class ClientApp:
     def __init__(self):
         self.filename = "inputImage.jpg"
         self.classifier = PredictionPipeline(self.filename)
 
-
 @app.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
-
-
-
 
 @app.route("/train", methods=['GET','POST'])
 @cross_origin()
@@ -33,8 +26,6 @@ def trainRoute():
     os.system("python main.py")
     # os.system("dvc repro")
     return "Training done successfully!"
-
-
 
 @app.route("/predict", methods=['POST'])
 @cross_origin()
@@ -44,8 +35,6 @@ def predictRoute():
     result = clApp.classifier.predict()
     return jsonify(result)
 
-
 if __name__ == "__main__":
     clApp = ClientApp()
-
-    app.run(host='0.0.0.0', port=8080) 
+    app.run(host='0.0.0.0', port=8080, debug=True)  # Added debug=True for auto-reloading
